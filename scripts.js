95f4de07f81a7ea849abcf31b53977dc23f1f720
ThmsRchrdsn <115042486+ThmsRchrdsn@users.nereply.github.com>
2022-12-19 16:33:11 +0000

    Font changed

--- scripts.js
+++ scripts.js
@@ -10,7 +10,7 @@ function fireEvent() {
   unsplashURL =
     "url(https://source.unsplash.com/" +
     windowDimensions +
-    "/?minimal/?bright)";
+    "/?minimal/?white)";
 
   document.getElementById("container").style.backgroundImage = unsplashURL;
 
