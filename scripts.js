50056d521914c4961f503c36ee4d09e7fab22929
ThmsRchrdsn <115042486+ThmsRchrdsn@users.nereply.github.com>
2023-01-10 17:31:46 +0000

    Random Fonts

--- scripts.js
+++ scripts.js
@@ -1,20 +1,23 @@
 let quote = document.querySelector("#quote");
 let author = document.querySelector("#author");
+let fonts = ["Ms Madi", "Satisfy", "Pacifico", "Great Vibes", "Sacramento", "Tangerine", "Alex Brush"]
 const url = "https://quotable.io/random";
 
 window.onload = fireEvent();
 
 function fireEvent() {
   let windowDimensions = window.innerWidth + "x" + window.innerHeight;
-
   // unsplashURL =
   //   "url(https://source.unsplash.com/" +
   //   windowDimensions +
   //   "/?minimal/?white)";
 
   unsplashURL = "url(https://source.unsplash.com/random/?minimal)";
-
   document.getElementById("container").style.backgroundImage = unsplashURL;
+
+  let font = fonts[Math.floor(Math.random() * fonts.length)];
+  document.getElementById("quote").style.fontFamily = font;
+  document.getElementById("author").style.fontFamily = font;
 
   fetch(url)
     .then((response) => {
